835accb182ea18b2b5c490a7c9178a3a2fb5992e
NoxFly <dorian.thivolle@gmail.com>
2018-09-03 11:36:17 +0200

    check game

--- functions/check.js
+++ functions/check.js
@@ -68,40 +68,7 @@ const check = function(msg, id, name, avatar) {
                         champion: 0,
                         wins: 0,
                         defeat: 0,
-                        owned: 0,
-                        params: {
-                            name: "Aatrox",
-                            armor: "33",
-                            attack: "60",
-                            critics: "10",
-                            health: "580",
-                            magicResist: "32.1",
-                            power: "0",
-                            role: "Combattant",
-                            type: "AD",
-                            spells: {
-                                Q: {
-                                    name : "The Darkin Blade",
-                                    damages : 50,
-                                    delay : 1
-                                },
-                                W: {
-                                    name : "Infernal Chains",
-                                    damages : 0,
-                                    delay : 1
-                                },
-                                E: {
-                                    name : "Umbral Dash",
-                                    damages : 50,
-                                    delay : 1
-                                },
-                                R: {
-                                    name : "World Ender",
-                                    damages : 500,
-                                    delay : 6
-                                }
-                            }
-                        }
+                        owned: 0
                     }
                 });
                 
