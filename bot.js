499bdeaa41163f0b5c6015b19a04ea7ff4b75975
NoxFly <dorian.thivolle@gmail.com>
2018-09-09 16:54:54 +0200

    test separate Ahri to others

--- bot.js
+++ bot.js
@@ -42,59 +42,117 @@ function startbot(params) {
 	const bot = new Discord.Client();
 	exportObj.bot = bot;
 
-	bot.on('ready',() => {
-		console.log(params.name+' ready !\n');
-		let m;
-		let members;
-		let activity = 1;
-		bot.user.setActivity(params.tag+'help | '+bot.guilds.size+' servers');
+	if(params.name=='caitlyn') {
+		bot.on('ready',() => {
+			console.log(params.name+' ready !\n');
+			bot.user.setActivity('with '+bot.guilds.size+' servers');
+		});
 
-		setInterval(function() {
-			activity = 1-activity;
-			Database.source().getData('profile', function(data) {
-				m = data.val();
-			});
+		bot.on('message', (msg) => {
+			if(msg.author.id===params.id || msg.author.bot==true) {
+				return;
+			}
+	
+			if(msg.content=='<@'+params.id+'>' || msg.content=='<@'+params.id+'> .') {
+				send(msg, 'Hey :P');
+				return;
+			}
+	
+			let id = msg.author.id;
+			let name = msg.author.username+'#'+msg.author.discriminator;
+			let avatar = msg.author.avatarURL;
+			check(msg, id, name, avatar);
 
-			setTimeout(function() {
-				try {
-					members = Object.keys(m).length;
-					if(activity) bot.user.setActivity(params.tag+'help | '+bot.guilds.size+' servers');
-					else bot.user.setActivity(params.tag+'help | '+members+' members');
-				} catch(error) {
-					console.log(error);
-					bot.user.setActivity(params.tag+'help | '+bot.guilds.size+' servers');
+			if(msg.channel.type!=='dm') {
+				let gid = msg.guild.id;
+				let gname = msg.guild.name;
+				let gowner = msg.guild.ownerID;
+				checkServer(gid, gname, gowner);
+				commands = require('./bots/'+params.name+'/basic.js');
+
+				let content = msg.content;
+				if(content.indexOf(params.tag)===0) {
+					for(let a=0; a<commands.length; a++) {
+						let command = commands[a];
+						if (find(content, command.name)) {
+							try {
+								let txt = command.result(msg);
+								if(txt=='not_find') {
+									return false;
+								} else {
+									send(msg, txt);
+								}
+							} catch(error) {
+								let embed = new Discord.RichEmbed()
+									.setAuthor('⚠️ Error ('+error.name+')')
+									.setColor(0xFFA500)
+									.setDescription('```'+error.message+'```');
+								send(msg, embed);
+							}
+							return false;
+						}
+					}
+					
+					send(msg,'Sorry, the command you wrote does not exist. :x:');
+					return false;
 				}
-			},Database.responseTime);
-		}, 600000);
-	});
+			} else {
+				send(msg, 'Commands are only availibles on server');
+			}
+		});
+	} else {
+		bot.on('ready',() => {
+			console.log(params.name+' ready !\n');
+			let m;
+			let members;
+			let activity = 1;
+			bot.user.setActivity(params.tag+'help | '+bot.guilds.size+' servers');
+	
+			setInterval(function() {
+				activity = 1-activity;
+				Database.source().getData('profile', function(data) {
+					m = data.val();
+				});
+	
+				setTimeout(function() {
+					try {
+						members = Object.keys(m).length;
+						if(activity) bot.user.setActivity(params.tag+'help | '+bot.guilds.size+' servers');
+						else bot.user.setActivity(params.tag+'help | '+members+' members');
+					} catch(error) {
+						console.log(error);
+						bot.user.setActivity(params.tag+'help | '+bot.guilds.size+' servers');
+					}
+				},Database.responseTime);
+			}, 600000);
+		});
 
-	bot.on('message', (msg) => {
-		if(msg.author.id===params.id || msg.author.bot==true) {
-			return;
-		}
-
-		if(msg.content=='<@'+params.id+'>' || msg.content=='<@'+params.id+'> .') {
-			send(msg, 'Hey :P');
-			return;
-		}
-
-		let id = msg.author.id;
-		let name = msg.author.username+'#'+msg.author.discriminator;
-		let avatar = msg.author.avatarURL;
-		check(msg, id, name, avatar);
-
-		let commands;
-
-		mod = [];
-		modules = [];
-
-		if(msg.channel.type!=='dm') {
-			let gid = msg.guild.id;
-			let gname = msg.guild.name;
-			let gowner = msg.guild.ownerID;
-			checkServer(gid, gname, gowner);
-
-			if(params.name=='ahri') {
+		bot.on('message', (msg) => {
+			if(msg.author.id===params.id || msg.author.bot==true) {
+				return;
+			}
+	
+			if(msg.content=='<@'+params.id+'>' || msg.content=='<@'+params.id+'> .') {
+				send(msg, 'Hey :P');
+				return;
+			}
+	
+			let id = msg.author.id;
+			let name = msg.author.username+'#'+msg.author.discriminator;
+			let avatar = msg.author.avatarURL;
+			check(msg, id, name, avatar);
+	
+			let commands;
+	
+			mod = [];
+			modules = [];
+	
+			if(msg.channel.type!=='dm') {
+				let gid = msg.guild.id;
+				let gname = msg.guild.name;
+				let gowner = msg.guild.ownerID;
+				checkServer(gid, gname, gowner);
+	
 				if(App[gid]===undefined || App[gid]['modules']===undefined) {
 					if(App[gid]===undefined) App[gid] = [];
 		
@@ -126,12 +184,9 @@ function startbot(params) {
 					exportObj.commands = modules;
 					commands = modules.concat(commands);
 				},timeExe);
-			} else {
 				timeExe = 0;
 				commands = require('./bots/'+params.name+'/basic.js');
-			}
-		} else {
-			if(params.name=='ahri') {
+			} else {
 				commands = require('./bots/'+params.name+'/basic.js');
 				mod = ['game','personal','social','utility'];
 				mod.forEach(name => {
@@ -145,56 +200,54 @@ function startbot(params) {
 				commands = modules.concat(commands);
 				timeExe = 0;
 			}
-		}
-
-		setTimeout(function() {
-			let content = msg.content;
-			if(content.indexOf(params.tag)===0) {
-				for(let a=0; a<commands.length; a++) {
-					let command = commands[a];
-					if (find(content, command.name)) {
-						try {
-							Database.setAuthor(msg.author);
-							Database.profile(msg.author.id);
-							let txt = command.result(msg);
-							if(txt=='not_find') {
-								return false;
-							} else {
-								send(msg, txt);
+	
+			setTimeout(function() {
+				let content = msg.content;
+				if(content.indexOf(params.tag)===0) {
+					for(let a=0; a<commands.length; a++) {
+						let command = commands[a];
+						if (find(content, command.name)) {
+							try {
+								Database.setAuthor(msg.author);
+								Database.profile(msg.author.id);
+								let txt = command.result(msg);
+								if(txt=='not_find') {
+									return false;
+								} else {
+									send(msg, txt);
+								}
+							} catch(error) {
+								let embed = new Discord.RichEmbed()
+									.setAuthor('⚠️ Error ('+error.name+')')
+									.setColor(0xFFA500)
+									.setDescription('```'+error.message+'```');
+								send(msg, embed);
 							}
-						} catch(error) {
-							let embed = new Discord.RichEmbed()
-								.setAuthor('⚠️ Error ('+error.name+')')
-								.setColor(0xFFA500)
-								.setDescription('```'+error.message+'```');
-							send(msg, embed);
+							return false;
 						}
-						return false;
+					}
+					
+					send(msg,'Sorry, the command you wrote does not exist. :x:');
+					return false;
+				}
+			}, timeExe+3);
+		});
+	
+		bot.on('guildCreate', (guild) => {
+			let id = guild.id;
+			let name = guild.name;
+			let owner = guild.ownerID;
+			checkServer(id, name, owner);
+	
+			let defaultChannel = "";
+			guild.channels.forEach((channel) => {
+				if(channel.type == "text" && defaultChannel == "") {
+					if(channel.permissionsFor(guild.me).has("SEND_MESSAGES")) {
+						defaultChannel = channel;
 					}
 				}
-				
-				send(msg,'Sorry, the command you wrote does not exist. :x:');
-				return false;
-			}
-		}, timeExe+3);
-	});
-
-	bot.on('guildCreate', (guild) => {
-		let id = guild.id;
-		let name = guild.name;
-		let owner = guild.ownerID;
-		checkServer(id, name, owner);
-
-		let defaultChannel = "";
-		guild.channels.forEach((channel) => {
-			if(channel.type == "text" && defaultChannel == "") {
-				if(channel.permissionsFor(guild.me).has("SEND_MESSAGES")) {
-					defaultChannel = channel;
-				}
-			}
-		});
-
-		if(params.name=='ahri') {
+			});
+	
 			let modules = '';
 			let end = ' - ';
 			fs.readdir('./bots/ahri/modules', function(err, items) {
@@ -215,35 +268,40 @@ function startbot(params) {
 				defaultChannel.send('Hey, I\'m Ahri !\nI advise you to read this :');
 				defaultChannel.send(embed);
 			});
-		}
-	});
+		});
+	
+		bot.on('messageReactionAdd', (reaction, user) => {
+			if(reaction.message.author.id == '463425089673887764'){
+				send(reaction.message,'reaction');
+				react(reactionCommands, reaction, user, reaction.message.content);
+			}
+		});
+	}
 
-	bot.on('messageReactionAdd', (reaction, user) => {
-		if(reaction.message.author.id == '463425089673887764'){
-			send(reaction.message,'reaction');
-			react(reactionCommands, reaction, user, reaction.message.content);
-		}
-	});
+	
+	/** */
+	
 
-	function send(msg,message){
-		if(message != null) {
+	function send(msg, message){
+		if(message!=null) {
 			msg.channel.send(message);
 		}
 	}
-	function find(content, txt,discussion = false){
-		if(discussion == false){
+
+	function find(content, txt, discussion = false){
+		if(discussion==false){
 		  return content.indexOf(txt)==params.tag.length;
-		}else{
+		} else {
 		  return content.indexOf(txt)==0;
 		}
 	}
 
-	function react(cmds,reaction,user,content) {
+	function react(cmds, reaction, user, content) {
 		for(let a = 0;a<cmds.length;a++) {
 			let command = cmds[a];
-			if(find(content, command.from, true) && reaction.emoji.name === command.emoji) {
-				let txt = command.result(reaction,user);
-				if(txt != null){
+			if(find(content, command.from, true) && reaction.emoji.name===command.emoji) {
+				let txt = command.result(reaction, user);
+				if(txt!=null){
 				  send(reaction.message, txt);
 				}
 			}
